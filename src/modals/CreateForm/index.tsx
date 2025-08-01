c2f2148179f0b49a17d7e026e81442b98b3a12b8
apelsin apelsin <abrikosechek@gmail.com>
2025-08-01 21:36:00 +0400

    CreateFormModel layout

--- src/modals/CreateForm/index.tsx
+++ src/modals/CreateForm/index.tsx
@@ -1,9 +1,1 @@
-import { Modal } from '@/shared/ui/Modal/ui/Modal'
-
-export const CreateFormModal = () => {
-  return (
-    <Modal title="Create new form">
-      <p>"Create form" modal</p>
-    </Modal>
-  )
-}
+export { CreateFormModal } from './CreateForm'
