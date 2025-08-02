676ffde5f285fc907bb60c5cea865fe6435cffb3
apelsin apelsin <abrikosechek@gmail.com>
2025-08-02 12:04:21 +0400

    aside links

--- src/features/Aside/ui/Aside.tsx
+++ src/features/Aside/ui/Aside.tsx
@@ -1,8 +1,35 @@
+import { NavLink } from 'react-router'
+import { ReactNode } from 'react'
 import { useModalStore } from '@/shared/model/Modal'
 import styles from './Aside.module.scss'
 import { Button } from '@/shared/ui'
 import { PlusIcon } from '@radix-ui/react-icons'
 import { CreateFormModal } from '@/modals/CreateForm'
+
+interface Props {
+  to: string
+  children: ReactNode
+}
+
+const AsideListItem = ({ to, children }: Props) => {
+  return (
+    <NavLink className={styles.asideListItem} to={`form/${to}`}>
+      {children}
+    </NavLink>
+  )
+}
+
+const links = [
+  {
+    name: 'first',
+  },
+  {
+    name: 'fie2q3wdrst',
+  },
+  {
+    name: '2314ewq',
+  },
+]
 
 export const Aside = () => {
   const setModal = useModalStore((state) => state.setModal)
@@ -22,7 +49,12 @@ export const Aside = () => {
           <PlusIcon />
         </Button>
       </div>
-      <div className={styles.aside__list}></div>
+
+      <div className={styles.aside__list}>
+        {links.map((link) => (
+          <AsideListItem to={link.name}>{link.name}</AsideListItem>
+        ))}
+      </div>
     </div>
   )
 }
