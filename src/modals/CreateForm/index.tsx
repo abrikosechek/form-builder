56cd261e00c31e24daa862feaa8a4325683cbb35
apelsin apelsin <abrikosechek@gmail.com>
2025-08-15 23:40:29 +0400

    moved modal root to index.tsx

--- src/modals/CreateForm/index.tsx
+++ src/modals/CreateForm/index.tsx
@@ -1,1 +1,25 @@
-export { CreateFormModal } from './CreateForm'
+import styles from './index.module.scss'
+import { Button, Input } from '@/shared/ui'
+import { Modal } from '@/shared/ui/Modal/ui/Modal'
+import { useState } from 'react'
+
+export const CreateFormModal = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  return (
+    <Modal title="Create new form">
+      <Input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Form title"
+      />
+
+      <div className={styles.buttons}>
+        <Button color="success" wide>
+          Create
+        </Button>
+        <Button color="grey" wide>Cancel</Button>
+      </div>
+    </Modal>
+  )
+}
