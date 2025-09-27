7980589b41f1e1e05dea5a1a498042fef5c7b875
abrikosechek <abrikosecheck@gmail.com>
2025-09-27 18:52:53 +0400

    Centered HomePage content

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,8 +1,13 @@
+import styles from './HomePage.module.scss'
 import { useFormsStore } from '@/entities/Forms'
 import { NoForms } from './NoForms'
 
 export const HomePage = () => {
   const { orderedForms } = useFormsStore()
 
-  return <>{orderedForms().length ? <h1>Main Page</h1> : <NoForms />}</>
+  return (
+    <div className={styles['home-page']}>
+      {orderedForms().length ? <h1>Main Page</h1> : <NoForms />}
+    </div>
+  )
 }
